5703bd7d47c7e0f169ba2ca98496a1aa0c589ca7
Chris Anh Thien <chrisanhthien@Chriss-MacBook-Pro.local>
2024-07-17 13:29:49 -0400

    Add dynamic values

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
+const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
+function genRandomInt(max) {
+  //This return a random number from 0, inclusive, to 1, exclusive
+  return Math.floor(Math.random()*(max+1));
+}
 function Header() {
+  const description = reactDescriptions[genRandomInt(2)];
   return (<header>
     <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
     <h1>React Essentials</h1>
     <p>
-      Fundamental React concepts you will need for almost any app you are
+      {description} React concepts you will need for almost any app you are
       going to build!
     </p>
   </header>);
